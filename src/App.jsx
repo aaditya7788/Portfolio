7c9b81fead6421ffacee2aec99712ee4be99fc90
{username} <aadityasahani78@gmail.com>
2025-06-07 00:58:14 +0530

    Fixed rename issue

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Hero from './components/Hero'
 
 import Portfolio from './components/Portfolio'
 import Skills from './components/Skills'
-import Contact from './components/COntact';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
